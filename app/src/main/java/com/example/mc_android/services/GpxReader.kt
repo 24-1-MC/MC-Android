06a5271e7bdcb80d6b18457931c5348e42f6d56a
wooooooooook <otsu9110@gmail.com>
2024-06-05 23:16:00 +0900

    [Feat] gpx파일을 xml parser로 읽어 GeoPoint객체배열로 변환하는 기능 추가

--- app/src/main/java/com/example/mc_android/services/GpxReader.kt
+++ app/src/main/java/com/example/mc_android/services/GpxReader.kt
@@ -2,24 +2,30 @@ package com.example.mc_android.services
 
 import android.content.Context
 import org.osmdroid.util.GeoPoint
-import java.io.BufferedReader
+import org.w3c.dom.Element
 import java.io.File
-import java.io.FileReader
+import javax.xml.parsers.DocumentBuilderFactory
 
-// gpx 파일을 BufferedRedaer를 통해 입력
-// GeoPoint객체를 MutableList에 저장하여 리턴
-class GpxReader(context: Context, fileName: String) {
-    private val fileName = fileName
+// gpx 파일을 BufferedRedaer를 통해 입력 - GeoPoint객체를 MutableList에 저장하여 리턴
+class GpxReader(context: Context, private val fileName: String) {
     private val file = File(context.filesDir, fileName).apply { createNewFile() }
-    private val reader = BufferedReader(FileReader(file))
+    private val output = mutableListOf<GeoPoint>()
 
+    fun parse(): MutableList<GeoPoint> {
+        val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+        val document = documentBuilder.parse(file)
+        document.documentElement.normalize()
 
+        val trkpts = document.getElementsByTagName("trkpt")
 
+        for (i in 0 until trkpts.length) {
+            val trkpt = trkpts.item(i) as Element
+            val lat = trkpt.getAttribute("lat").toDouble()
+            val lon = trkpt.getAttribute("lon").toDouble()
+            val ele = trkpt.getElementsByTagName("ele").item(0).textContent.toDouble()
+            output.add(GeoPoint(lat, lon, ele))
+        }
 
-
-//    	public GeoPoint(final double aLatitude, final double aLongitude, final double aAltitude) {
-//		this.mLatitude = aLatitude;
-//		this.mLongitude = aLongitude;
-//		this.mAltitude = aAltitude;
-//	}
+        return output
+    }
 }
