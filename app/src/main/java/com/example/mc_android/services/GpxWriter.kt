f3e379ad8ac4b21af9e06b9b3aeaddcbffe8263e
liberaldev <j0321@mju.ac.kr>
2024-06-11 23:04:28 +0900

    GpxWriter.kt 결함점 수정

--- app/src/main/java/com/example/mc_android/services/GpxWriter.kt
+++ app/src/main/java/com/example/mc_android/services/GpxWriter.kt
@@ -1,23 +1,25 @@
 package com.example.mc_android.services
 
 import android.content.Context
+import android.content.Intent
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.time.Instant
+import java.time.ZoneId
 
-class GpxWriter(context: Context) {
-    // 현재 시간 값을 파일 이름으로 사용
-    private var currentTime = LocalDateTime.now()
-    private val fileName = "${formatFileName(currentTime)}.gpx"
+class GpxWriter(context: Context, startTimeIsoTimestamp: String) {
+    // 현재 타임스탬프를 파일 이름으로 사용
+    private val fileName = "${startTimeIsoTimestamp}.gpx"
     private val file = File(context.filesDir, fileName).apply { createNewFile() }
     private val writer = BufferedWriter(FileWriter(file))
 
     fun initialize() {
         writer.apply {
             append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-            append("<gpx version=\"1.1\" creator=\"made_by_jpark/한결/지수/성준\">\n")
+            append("<gpx version=\"1.1\" creator=\"made_by_jpark/한결/지수/SJ\">\n")
             append("<trk>\n")
             append("  <name>OutdoorRunning</name>\n")
             append("  <trkseg>\n")
@@ -25,11 +27,14 @@ class GpxWriter(context: Context) {
     }
 
     fun append(latitude: Double, longitude: Double, altitude: Double) {
-        currentTime = LocalDateTime.now()
+        val currentTime = Instant.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+            .withZone(ZoneId.of("UTC"))
+        val formattedTime = formatter.format(currentTime)
         writer.apply {
             append("    <trkpt lat=\"$latitude\" lon=\"$longitude\">\n")
             append("      <ele>$altitude</ele>\n")
-            append("      <time>${formatTime(currentTime)}</time>\n")
+            append("      <time>${formattedTime}</time>\n")
             append("    </trkpt>\n")
         }
     }
@@ -45,17 +50,6 @@ class GpxWriter(context: Context) {
 
     fun getFileName(): String {
         return fileName
-    }
-
-
-
-    private fun formatTime(currentTime: LocalDateTime): String {
-        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
-        return currentTime.format(formatter)
-    }
-    private fun formatFileName(currentTime: LocalDateTime): String {
-        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd_HHmmss")
-        return currentTime.format(formatter)
     }
 }
 
