90a075eaeadef68d48c7f8863568ba0077b599dd
liberaldev <j0321@mju.ac.kr>
2024-06-12 13:51:27 +0900

    Revert "[FIX] 이동거리가 너무 적은 경우 날씨 데이터 못받아오는 문제 수정"
    This reverts commit 0b5ea99e

--- app/src/main/java/com/example/mc_android/services/WeatherService.kt
+++ app/src/main/java/com/example/mc_android/services/WeatherService.kt
@@ -1,9 +1,5 @@
 package com.example.mc_android.services
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.NetworkCapabilities
-import android.os.Build
 import com.google.gson.Gson
 import com.google.gson.JsonObject
 import okhttp3.OkHttpClient
@@ -14,12 +10,7 @@ data class WeatherInfo(val icon: String, val temperature: Int, val humidity: Int
 // open weather map api키
 private val key = "754918bb8d09858d91d5100163aa9c92"
 
-fun getWeather(context: Context, latitude: Double, longitude: Double): WeatherInfo? {
-    if (!isNetworkAvailable(context)) {
-        // 네트워크에 연결되어 있지 않은 경우 null 반환
-        return WeatherInfo("99", 99, 99)
-    }
-
+fun getWeather(latitude: Double, longitude: Double): WeatherInfo? {
     val client = OkHttpClient()
     // query
     val url = "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$key&units=metric"
@@ -43,24 +34,5 @@ fun getWeather(context: Context, latitude: Double, longitude: Double): WeatherIn
             ?.get("humidity")?.asInt ?: return null
 
         return WeatherInfo(icon, temperature, humidity)
-    }
-}
-
-fun isNetworkAvailable(context: Context): Boolean {
-    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        val network = connectivityManager.activeNetwork ?: return false
-        val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
-
-        return when {
-            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
-            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
-            activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
-            else -> false
-        }
-    } else {
-        val networkInfo = connectivityManager.activeNetworkInfo
-        return networkInfo != null && networkInfo.isConnected
     }
 }
