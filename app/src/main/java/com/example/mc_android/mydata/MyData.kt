33b173306681490b1bd4d2d367d337991942ef9d
liberaldev <j0321@mju.ac.kr>
2024-06-13 13:43:56 +0900

    DB 스키마 수정

--- app/src/main/java/com/example/mc_android/mydata/MyData.kt
+++ app/src/main/java/com/example/mc_android/mydata/MyData.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "my_data")
 data class MyData(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val startAt: String,
