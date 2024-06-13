e3b7db439d6f708cbbde333a665439ea7b1f4615
liberaldev <j0321@mju.ac.kr>
2024-06-13 15:43:04 +0900

    달력 한국어로 나오게 수정

--- app/src/main/java/com/example/mc_android/MainActivity.kt
+++ app/src/main/java/com/example/mc_android/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mc_android
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.os.Bundle
 import android.text.InputType
 import android.text.TextUtils.replace
@@ -20,9 +21,19 @@ import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private var backPressedTime: Long = 0 // 뒤로 가기 버튼이 눌린 시간을 기록할 변수
+
+    override fun attachBaseContext(newBase: Context) {
+        val locale = Locale("ko")
+        Locale.setDefault(locale)
+        val config = Configuration()
+        config.setLocale(locale)
+        val context = newBase.createConfigurationContext(config)
+        super.attachBaseContext(context)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
